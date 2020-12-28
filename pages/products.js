7b4a67db2c9da11010a5a657baf7a4d5512421bf
Cameron Green <cameronagreen2000@gmail.com>
2020-12-28 00:09:35 -0500

    Added images to products page

--- pages/products.js
+++ pages/products.js
@@ -3,6 +3,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/Layout";
 import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import Image from 'next/image';
 
 
 const Produts = (props) => {
@@ -13,10 +14,7 @@ const Produts = (props) => {
             {props.data.map((elem) => {
               return (
                 <Card className='mx-4 my-3' style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src="https://www.bowerswilkins.com/sites/default/files/2019-09/Low--PX7%20Front%20-%20Space%20Grey.jpg"
-                  />
+                  <Image src={'/img/' + elem.GraphicName} alt='Picture of headphone' width={500} height={500}></Image>
                   <Card.Body>
                     <Card.Title>{elem.ProductName}</Card.Title>
                     <Card.Text>{elem.Description}</Card.Text>
